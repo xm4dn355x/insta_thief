3d7a136e25255ea0cbc523588f5bcb3a128b912f
Michael Nikitenko <xm4dn355x@gmail.com>
2022-04-27 01:40:28 +0300

    pep8

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 import py2exe
 
 setup(
-    windows=[{"script":"Debug.py"}],
-    options={"py2exe": {"includes":["sip"]}}
+    windows=[{"script": "Debug.py"}],
+    options={"py2exe": {"includes": ["sip"]}}
 )
